d7bae576c0a33dfe4c675533a5b1464d6ca5a74e
Nguyen Duc Trung <92ngductrung@gmail.com>
2015-07-02 11:59:18 +0700

    Oops, we didn't want this commit

---
+++ array.rb
@@ -0,0 +1,61 @@
+# names =Array.new(21) 
+# puts names.size
+# puts names.length
+
+# names = Array.new(4, "mac")
+# puts "#{names}"
+
+# nums =Array.new(10) {|e| e = e*2}
+# puts "#{nums}"
+
+# nums = Array.[](1, 2, 3, 4, 5)
+# puts "#{nums}"
+# nums = Array[1, 2, 3, 4,5]
+# puts "#{nums}"
+
+digits = Array(1..9)
+num = digits.at(0)
+puts "#{digits}"
+puts "#{num}"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
